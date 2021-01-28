a1a9895e897ff258ac22dbdcd29880534ef0211d
Luiz Gustavo <lgpossatto@gmail.com>
2021-01-28 17:07:59 -0300

    fetch api and site rendering working great

--- script/main.js
+++ script/main.js
@@ -1,14 +1,319 @@
-/* 
-const getWeather = async () => {
-  console.log("ok");
-  const resp = await fetch("");
-  const ret = await resp.json();
-
-  return ret;
+let test = {
+  lat: 33.4418,
+  lon: -94.0377,
+  timezone: "America/Chicago",
+  timezone_offset: -21600,
+  current: {
+    dt: 1611858035,
+    sunrise: 1611839674,
+    sunset: 1611877414,
+    temp: 278.82,
+    feels_like: 274.59,
+    pressure: 1037,
+    humidity: 56,
+    dew_point: 271.02,
+    uvi: 4.17,
+    clouds: 1,
+    visibility: 10000,
+    wind_speed: 2.74,
+    wind_deg: 68,
+    weather: [
+      {
+        id: 800,
+        main: "Clear",
+        description: "clear sky",
+        icon: "01d",
+      },
+    ],
+  },
+  daily: [
+    {
+      dt: 1611856800,
+      sunrise: 1611839674,
+      sunset: 1611877414,
+      temp: {
+        day: 278.82,
+        min: 271.97,
+        max: 281.66,
+        night: 275.74,
+        eve: 278.45,
+        morn: 272.19,
+      },
+      feels_like: {
+        day: 274.59,
+        night: 272.16,
+        eve: 275.51,
+        morn: 267.72,
+      },
+      pressure: 1037,
+      humidity: 56,
+      dew_point: 271.02,
+      wind_speed: 2.74,
+      wind_deg: 68,
+      weather: [
+        {
+          id: 800,
+          main: "Clear",
+          description: "clear sky",
+          icon: "01d",
+        },
+      ],
+      clouds: 1,
+      pop: 0,
+      uvi: 4.17,
+    },
+    {
+      dt: 1611943200,
+      sunrise: 1611926037,
+      sunset: 1611963873,
+      temp: {
+        day: 283.35,
+        min: 274.34,
+        max: 285.56,
+        night: 281.4,
+        eve: 281.37,
+        morn: 274.34,
+      },
+      feels_like: {
+        day: 279.3,
+        night: 277.4,
+        eve: 278.14,
+        morn: 271.07,
+      },
+      pressure: 1026,
+      humidity: 61,
+      dew_point: 276.19,
+      wind_speed: 3.65,
+      wind_deg: 172,
+      weather: [
+        {
+          id: 803,
+          main: "Clouds",
+          description: "broken clouds",
+          icon: "04d",
+        },
+      ],
+      clouds: 76,
+      pop: 0,
+      uvi: 4.05,
+    },
+    {
+      dt: 1612029600,
+      sunrise: 1612012399,
+      sunset: 1612050332,
+      temp: {
+        day: 287.61,
+        min: 281.76,
+        max: 292.37,
+        night: 283.52,
+        eve: 288.38,
+        morn: 284.22,
+      },
+      feels_like: {
+        day: 282.99,
+        night: 279.43,
+        eve: 283.41,
+        morn: 279.91,
+      },
+      pressure: 1008,
+      humidity: 91,
+      dew_point: 286.29,
+      wind_speed: 7.93,
+      wind_deg: 187,
+      weather: [
+        {
+          id: 500,
+          main: "Rain",
+          description: "light rain",
+          icon: "10d",
+        },
+      ],
+      clouds: 100,
+      pop: 1,
+      rain: 3.56,
+      uvi: 1.78,
+    },
+    {
+      dt: 1612116000,
+      sunrise: 1612098760,
+      sunset: 1612136790,
+      temp: {
+        day: 283.4,
+        min: 278.75,
+        max: 286.07,
+        night: 278.75,
+        eve: 281.8,
+        morn: 278.84,
+      },
+      feels_like: {
+        day: 278.38,
+        night: 274.6,
+        eve: 277.6,
+        morn: 274.49,
+      },
+      pressure: 1020,
+      humidity: 66,
+      dew_point: 277.37,
+      wind_speed: 5.34,
+      wind_deg: 323,
+      weather: [
+        {
+          id: 802,
+          main: "Clouds",
+          description: "scattered clouds",
+          icon: "03d",
+        },
+      ],
+      clouds: 41,
+      pop: 0,
+      uvi: 3.49,
+    },
+    {
+      dt: 1612202400,
+      sunrise: 1612185119,
+      sunset: 1612223249,
+      temp: {
+        day: 284.09,
+        min: 275.67,
+        max: 286.15,
+        night: 279.56,
+        eve: 281.97,
+        morn: 275.67,
+      },
+      feels_like: {
+        day: 279.54,
+        night: 276.69,
+        eve: 278.95,
+        morn: 271.36,
+      },
+      pressure: 1026,
+      humidity: 50,
+      dew_point: 274.31,
+      wind_speed: 3.86,
+      wind_deg: 343,
+      weather: [
+        {
+          id: 800,
+          main: "Clear",
+          description: "clear sky",
+          icon: "01d",
+        },
+      ],
+      clouds: 0,
+      pop: 0,
+      uvi: 3.91,
+    },
+    {
+      dt: 1612288800,
+      sunrise: 1612271477,
+      sunset: 1612309708,
+      temp: {
+        day: 284.13,
+        min: 276.29,
+        max: 286.09,
+        night: 279.78,
+        eve: 282.08,
+        morn: 276.29,
+      },
+      feels_like: {
+        day: 280.48,
+        night: 276.01,
+        eve: 278.86,
+        morn: 273.2,
+      },
+      pressure: 1023,
+      humidity: 57,
+      dew_point: 275.99,
+      wind_speed: 3.01,
+      wind_deg: 146,
+      weather: [
+        {
+          id: 804,
+          main: "Clouds",
+          description: "overcast clouds",
+          icon: "04d",
+        },
+      ],
+      clouds: 86,
+      pop: 0,
+      uvi: 4,
+    },
+    {
+      dt: 1612375200,
+      sunrise: 1612357833,
+      sunset: 1612396166,
+      temp: {
+        day: 286.73,
+        min: 277.95,
+        max: 287.18,
+        night: 286.97,
+        eve: 286.4,
+        morn: 277.95,
+      },
+      feels_like: {
+        day: 281.99,
+        night: 282.12,
+        eve: 282.19,
+        morn: 273.37,
+      },
+      pressure: 1013,
+      humidity: 68,
+      dew_point: 281.01,
+      wind_speed: 6.03,
+      wind_deg: 169,
+      weather: [
+        {
+          id: 803,
+          main: "Clouds",
+          description: "broken clouds",
+          icon: "04d",
+        },
+      ],
+      clouds: 82,
+      pop: 0.28,
+      uvi: 4,
+    },
+    {
+      dt: 1612461600,
+      sunrise: 1612444188,
+      sunset: 1612482624,
+      temp: {
+        day: 280.37,
+        min: 276.92,
+        max: 290,
+        night: 276.92,
+        eve: 279.3,
+        morn: 288.82,
+      },
+      feels_like: {
+        day: 274.41,
+        night: 271.39,
+        eve: 273.32,
+        morn: 285.54,
+      },
+      pressure: 1005,
+      humidity: 85,
+      dew_point: 278.08,
+      wind_speed: 6.87,
+      wind_deg: 295,
+      weather: [
+        {
+          id: 500,
+          main: "Rain",
+          description: "light rain",
+          icon: "10d",
+        },
+      ],
+      clouds: 100,
+      pop: 1,
+      rain: 4.4,
+      uvi: 4,
+    },
+  ],
 };
-console.log(getWeather());
-*/
 
+let hasFetch = true;
+// clock
 const getTime = () => {
   const weekDays = [
     "Domingo",
@@ -37,4 +342,106 @@ const handleClock = () => {
   }<span>${newTime[2]}, ${newTime[3]}</span>`;
 };
 
-const timeRunning = setInterval(handleClock, 5000);
+const removeError = () => {
+  document.getElementById("error").style.display = "none";
+};
+
+const seeDemo = () => {
+  removeError();
+  removeLoading();
+};
+
+const showError = () => {
+  document.getElementById("error").style.display = "block";
+  removeWelcome();
+};
+
+// at loading
+const removeWelcome = () => {
+  document.getElementById("welcome-div").style.display = "none";
+};
+
+const removeLoading = () => {
+  document.getElementById("main-container").classList.remove("loading");
+};
+
+const removeAll = () => {
+  removeWelcome();
+  removeLoading();
+  removeError();
+};
+
+const handleObj = (obj) => {
+  const {
+    temp,
+    feels_like,
+    humidity,
+    clouds,
+    wind_speed,
+    weather,
+  } = obj.current;
+  const {
+    temp: { min, max },
+  } = obj.daily[0];
+
+  const kelvinToC = (value) => value - 273.15;
+
+  const weatherImg = document.getElementById("weather-img");
+  weatherImg.firstElementChild.src = "/images/cloudy.png";
+  weatherImg.lastElementChild.textContent = `${kelvinToC(temp).toFixed(1)}º`;
+
+  const weatherInfo = document.getElementById("weather-info").children;
+  weatherInfo[0].textContent = `${weather[0].description}`;
+  weatherInfo[1].textContent = `Sensação Termica: ${kelvinToC(
+    feels_like
+  ).toFixed(2)}º`;
+  weatherInfo[2].textContent = `Máxima: ${kelvinToC(max).toFixed(2)}º`;
+  weatherInfo[3].textContent = `Mínima: ${kelvinToC(min).toFixed(2)}º`;
+  weatherInfo[4].textContent = `Humidade: ${humidity}%`;
+  weatherInfo[5].textContent = `Nebulosidade: ${clouds}%`;
+  weatherInfo[6].textContent = `Velocidade do Vento: ${wind_speed.toFixed(
+    2
+  )}m/s`;
+};
+
+const getWeather = async () => {
+  try {
+    const resp = await fetch(
+      "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,minutely&appid=b60cdd3bc4b5e2602c1e98fa10709a32"
+    );
+    const ret = await resp.json();
+
+    handleObj(ret);
+    removeAll();
+  } catch (error) {
+    hasFetch = false;
+  }
+};
+
+const welcome = () => {
+  const welcomeDiv = document.getElementById("welcome-div");
+
+  //getWeather();
+  hasFetch = true;
+  handleObj(test);
+  removeAll();
+
+  setTimeout(() => {
+    welcomeDiv.lastElementChild.style.visibility = "visible";
+    welcomeDiv.lastElementChild.style.opacity = "1";
+  }, 1000);
+
+  setTimeout(() => {
+    if (!hasFetch) {
+      showError();
+    }
+  }, 5000);
+};
+
+// at start
+(() => {
+  handleClock();
+  setInterval(handleClock, 5000);
+  document.getElementById("btn-see-demo").addEventListener("click", seeDemo);
+  welcome();
+})();
