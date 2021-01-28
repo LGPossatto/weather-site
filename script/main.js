984e5fdf58298f6c0e5498e69b26abccab1baab9
Luiz Gustavo <lgpossatto@gmail.com>
2021-01-28 11:37:49 -0300

    mod weather css

--- script/main.js
+++ script/main.js
@@ -8,3 +8,33 @@ const getWeather = async () => {
 };
 console.log(getWeather());
 */
+
+const getTime = () => {
+  const weekDays = [
+    "Domingo",
+    "Segunda-Feira",
+    "Terça-Feira",
+    "Quarta-Feira",
+    "Quinta-Feira",
+    "Sexta-Feira",
+    "Sabado",
+  ];
+  const newTime = new Date();
+  const newHour = newTime.getHours();
+  const newMinute = newTime.getMinutes();
+  const newDay = newTime.getDate();
+  const newWeekDay = weekDays[newTime.getDay()];
+
+  return [newHour, newMinute, newDay, newWeekDay];
+};
+
+const weatherTime = document.getElementById("weather-time");
+const handleClock = () => {
+  let newTime = getTime();
+
+  weatherTime.innerHTML = `${newTime[0]}:${
+    newTime[1] > 9 ? newTime[1] : `0${newTime[1]}`
+  }<span>${newTime[2]}, ${newTime[3]}</span>`;
+};
+
+const timeRunning = setInterval(handleClock, 5000);
